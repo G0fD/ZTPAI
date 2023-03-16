0cd2f6c666e860885a3c52afeebfe4019b5aa699
Jakub <gofdark231@gmail.com>
2023-03-16 14:18:53 +0100

    Initial commit

---
+++ src/main/java/com/company/shipify/ShipifyApplication.java
@@ -0,0 +1,13 @@
+package com.company.shipify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShipifyApplication.class, args);
+    }
+
+}
